114e4c9f5b7476a84291d49174c5e1ef5e778e6a
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-07 08:50:36 -0500

    prelim docs

--- bit-docs.js
+++ bit-docs.js
@@ -1,3 +1,13 @@
+/**
+ * @module {function} bit-docs-prettify
+ * @parent plugins
+ *
+ * @description A bit-docs plugin that makes source-code snippets in HTML prettier.
+ *
+ * @body
+ *
+ * TBD
+ */
 module.exports = function(bitDocs){
     var pkg = require("./package.json");
     var dependencies = {};

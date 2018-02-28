f9ba9b41a58556ad27024c6a84bd39a73d7e9923
Christopher Baker <christopher@bitovi.com>
2018-02-27 19:39:33 -0800

    add fix for zombie

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 var generate = require("bit-docs-generate-html/generate");
-var Q = require("q");
 var path = require("path");
+var fs = require("fs");
 var assert = require("assert");
 
 var Browser = require("zombie");
 var connect = require("connect");
+
+var zombieFixes = "Object.defineProperty(HTMLElement.prototype, 'classList', { get: function() { var parent = this; var classList = parent.className.split(' '); classList.contains = classList.includes; classList.add = function(token) { this.push(token); parent.className = this.join(' '); }; return classList; } });\n\n";
 
 var open = function(url, callback, done) {
 	var server = connect().use(connect.static(path.join(__dirname))).listen(8081);
@@ -27,7 +29,7 @@ describe("bit-docs-prettify", function() {
 	it("basics work", function(done) {
 		this.timeout(30000);
 
-		var docMap = Q({
+		var docMap = Promise.resolve({
 			index: {
 				name: "index",
 				body: "```javascript\nvar str = 'hello world';\n```\n\n```css\nbody {\n  margin: 0;\n  background: purple;\n}\n```\n\n```shell\npwd\n```\n\n```\n// some misc code\n```"
@@ -45,13 +47,16 @@ describe("bit-docs-prettify", function() {
 			forceBuild: true
 		})
 			.then(function() {
+				var stealPath = path.join(__dirname, "temp", "static", "node_modules", "steal", "steal.production.js");
+				fs.writeFileSync(stealPath, zombieFixes + fs.readFileSync(stealPath, "utf8"));
+
 				open("temp/index.html", function(browser, close) {
 					var codes = browser.window.document.getElementsByTagName("code");
 
 					for (var i = 0; i < codes.length; i++) {
 						assert.ok(codes[i].className.includes("language-"), "has a language");
 
-						if (codes[i].parentNode.nodeName === "pre") {
+						if (codes[i].parentNode.nodeName === "PRE") {
 							assert.ok(codes[i].parentNode.className.includes("language-"), "parent has a language");
 						}
 					}

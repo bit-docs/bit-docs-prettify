902a4e276c88e37e57d88886ca11f24c85fd9f57
Christopher Baker <christopher@bitovi.com>
2018-02-28 17:56:59 -0800

    refactor collapse and collapse line numbers

--- prism-collapse.js
+++ prism-collapse.js
@@ -9,38 +9,55 @@ function hasClass(element, className) {
   return (" " + element.className + " ").replace(/[\n\t]/g, " ").indexOf(className) > -1
 }
 
-function collapseLines(pre, config) {
-	var code = pre.children[0];
-	var lines = code.innerHTML.split('\n');
-	lines = lines.map(function(line, index) {
-		if (index === lines.length - 1) {
-			return line;
-		}
-
-		return line + '\n';
-	});
-
+function collapseLines(pre, config, hasLineNumbers) {
 	var inserts = [];
+
 	var ranges = config.split(',');
 	for (var i = 0; i < ranges.length; i++) {
 		var parts = ranges[i].split('-');
 
-		inserts.push([ +parts[0], '<div class="collapse collapsed"><div class="collapse-code">' ]);
-		inserts.push([ +parts[1], '</div></div>' ]);
+		inserts.push([ +parts[0], '<div class="collapse collapsed" data-index="' + i + '"><div class="collapse-code">', '<div class="collapse collapsed" data-index="' + i + '"><div class="collapse-lines">' ]);
+		inserts.push([ +parts[1] + 1, '</div></div>', '</div></div>' ]);
 	}
 
 	inserts.sort(function (a, b) {
 		return b[0] - a[0];
 	});
 
-	for (var i = 0; i < inserts.length; i++) {
-		var line = inserts[i][0] - 1;
-		var content = inserts[i][1];
 
-		lines.splice(line, 0, content);
+	var codeContainer = pre.children[0];
+
+	var numbersContainer = null;
+	var numbers = null;
+	if (hasLineNumbers) {
+		numbersContainer = codeContainer.lastChild;
+		numbersContainer.remove();
+
+		numbers = numbersContainer.innerHTML.split('<span></span>');
 	}
 
-	code.innerHTML = lines.join('');
+	var code = codeContainer.innerHTML.split('\n');
+	code = code.map(function(line, index) {
+		if (index === code.length - 1) {
+			return line;
+		}
+
+		return line + '\n';
+	});
+
+	for (var i = 0; i < inserts.length; i++) {
+		var line = inserts[i][0] - 1;
+
+		code.splice(line, 0, inserts[i][1]);
+		numbers[line] += inserts[i][2];
+	}
+
+	codeContainer.innerHTML = code.join('');
+
+	if (hasLineNumbers) {
+		numbersContainer.innerHTML = numbers.join('<span></span>');
+		codeContainer.appendChild(numbersContainer);
+	}
 }
 
 Prism.hooks.add('complete', function completeHook(env) {
@@ -57,12 +74,18 @@ Prism.hooks.add('complete', function completeHook(env) {
 	if (hasClass(pre, 'line-numbers') && hasLineNumbers && !isLineNumbersLoaded) {
 		Prism.hooks.add('line-numbers', completeHook);
 	} else {
-		collapseLines(pre, config);
+		collapseLines(pre, config, hasLineNumbers);
 	}
 });
 
 $('body').on('click', 'pre code .collapse.collapsed', function() {
-	$(this).removeClass('collapsed');
+	var parent = $(this).closest('code');
+	var index = $(this).attr('data-index');
+
+	parent
+		.find('.collapse[data-index=' + index + ']')
+		.removeClass('collapsed')
+	;
 });
 
 })();

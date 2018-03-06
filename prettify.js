734bac4e3613742bbb23f7b905f5d9a448c6dd58
Christopher Baker <christopher@hmudesign.com>
2018-03-05 17:47:34 -0800

    Prism fixes (#17)
    * create prism-collapse plugin
    * fix prism styles, add line-highlight, trim trailing newlines, highlight html template strings
    * refactor collapse and collapse line numbers
    * collapse handles highlights
    * disable prism auto-run and fix highlight color
    * do not upgrade naked code
    * remove highlight and collapse code (moved to bit-docs-html-highlight-line)

--- prettify.js
+++ prettify.js
@@ -1,49 +1,62 @@
 require("./prettify.less");
 
+require("./prism-config");
 var Prism = require("prismjs");
-require("prismjs/plugins/line-numbers/prism-line-numbers.js");
-require("prismjs/plugins/previewers/prism-previewers.js");
-require("prismjs/plugins/command-line/prism-command-line.js");
-
 require("prismjs/themes/prism-coy.css");
+
+require("prismjs/plugins/line-numbers/prism-line-numbers");
 require("prismjs/plugins/line-numbers/prism-line-numbers.css");
+
+require("prismjs/plugins/previewers/prism-previewers");
 require("prismjs/plugins/previewers/prism-previewers.css");
+
+require("prismjs/plugins/command-line/prism-command-line");
 require("prismjs/plugins/command-line/prism-command-line.css");
 
-/**
- * @parent bit-docs-prettify/static
- * @module {function} bit-docs-prettify/prettify.js
- *
- * Main front end JavaScript file for static portion of this plugin.
- *
- * @signature `prettyPrint()`
- *
- * Finds all `<code>` elements on the page and adds the `prettyprint` class
- * before executing the required pretty print engine.
- *
- * Also requires [bit-docs-prettify/prettify.less].
- */
-module.exports = function(){
+require("prismjs/plugins/toolbar/prism-toolbar");
+require("prismjs/plugins/toolbar/prism-toolbar.css");
+require("prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard");
+
+Prism.languages.insertBefore('javascript', 'template-string', {
+	'html-template-string': {
+		pattern: /`(?:[\s\S])*<[a-z-]+(?:\s+[^<>]*)?>(?:[\s\S])*`/,
+		inside: {
+			'interpolation': {
+				pattern: /\$\{[^}]+\}/,
+				inside: {
+					'interpolation-punctuation': {
+						pattern: /^\$\{|\}$/,
+						alias: 'punctuation'
+					},
+					rest: Prism.languages.javascript
+				}
+			},
+			rest: Prism.languages.html
+		}
+	}
+});
+
+module.exports = function() {
 	var codes = document.getElementsByTagName("code");
 	for (var i = 0; i < codes.length; i++) {
 		var code = codes[i];
 
+		if (code.textContent.slice(-1) === "\n") {
+			code.textContent = code.textContent.slice(0, -1);
+		}
+
 		if (code.parentNode.nodeName.toUpperCase() === "PRE") {
 			code.parentNode.className += " line-numbers";
+
+			if (code.className.includes("language-shell")) {
+				code.parentNode.className += " command-line";
+			}
 
 			if (!code.className.includes("language-")) {
 				code.className += " language-unknown";
 			}
-
-			if (code.className.includes("language-shell")) {
-				code.parentNode.className += " command-line";
-
-				if (code.textContent.slice(-1) === "\n") {
-					code.textContent = code.textContent.slice(0, -1);
-				}
-			}
 		}
 	}
 
-	Prism.highlightAll();
-}
+	window.requestAnimationFrame(Prism.highlightAll);
+};
